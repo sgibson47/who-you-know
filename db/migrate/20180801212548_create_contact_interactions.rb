58c17de8073360771374957def494b4e27a63d0e
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:27:47 -0500

    add content to #change that will create an contact_interactions table with columns for the interaction_id and contact_id

--- db/migrate/20180801212548_create_contact_interactions.rb
+++ db/migrate/20180801212548_create_contact_interactions.rb
@@ -1,4 +1,8 @@
 class CreateContactInteractions < ActiveRecord::Migration
   def change
+    create_table :contact_interactions do |t|
+      t.integer :contact_id
+      t.integer :interaction_id
+    end
   end
 end
