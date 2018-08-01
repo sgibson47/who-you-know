e977e019a8fb9afc3743f5d39357604fdfc7a1cf
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:43:32 -0500

    add methods to set and authenticate against a BCrypt password with has_secure_password; add presence validations for username, email and password; add uniqueness validation for username"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_many :interactions
   has_many :notes, through: :contacts
   has_many :notes, through: :interactions
+
+  has_secure_password
+
+  validates :email, :username, :password, presence: {message: "In order to signup, you must include an email, username, and password."}
+  validates :username, uniqueness: {message: "That username has been taken. Please try another."}
 end
