feefc698efc438aff33f83997999fa1f51b63e5c
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:46:10 -0500

    add presence validation for content

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :contact
   belongs_to :interaction
+
+  validates :content, presence: {message: "A message must have content. Please add to the note."}
 end
