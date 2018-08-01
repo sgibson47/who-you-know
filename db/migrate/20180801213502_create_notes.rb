535c9cd65bf596a17eeb172944fdcc4aefc8189d
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:37:34 -0500

    add content to #change that will create a notes table with columns for the interaction_id, contact_id, content, and timestamps

--- db/migrate/20180801213502_create_notes.rb
+++ db/migrate/20180801213502_create_notes.rb
@@ -1,4 +1,11 @@
 class CreateNotes < ActiveRecord::Migration
   def change
+    create_table :notes do |t|
+      t.integer :cotnact_id
+      t.integer :interaction_id
+      t.string :content
+
+      t.timestamps null: false
+    end
   end
 end
