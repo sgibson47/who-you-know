00136ced7f75a4c5d0a856858722b05984c76640
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:06:13 -0500

    correct Interaction class's association from has_many interactions to has_many contacts

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,5 +1,5 @@
 class Interaction < ActiveRecord::Base
   belongs_to :user
   has_many :contact_interactions
-  has_many :interactions, through: :contact_interactions
+  has_many :contacts, through: :contact_interactions
 end
