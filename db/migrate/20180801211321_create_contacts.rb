1054e935e7f347daa105594171c56594bfc39580
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:20:23 -0500

    add content to #change that will create a contacts table with columns for the contact's name, user_id, and timestamps

--- db/migrate/20180801211321_create_contacts.rb
+++ db/migrate/20180801211321_create_contacts.rb
@@ -1,4 +1,10 @@
 class CreateContacts < ActiveRecord::Migration
   def change
+    create_table :contacts do |t|
+      t.string :name
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
   end
 end
