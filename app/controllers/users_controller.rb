94b7c7f7b2ecf8db172918b22d6ab451ae1ee4e7
sgibson47 <sgibson47@gmail.com>
2018-08-01 17:01:31 -0500

    add get '/signup' route that renders a user's show page if she is logged in and renders the signup view if not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      erb :'users/show', , locals: {message: "Please Log Out to create a new user account."}
+    else
+      erb :'/users/signup'
+    end
+  end
   
 end
