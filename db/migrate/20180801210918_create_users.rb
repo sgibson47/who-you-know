972a7e05d74e686a4a98a3ee088b795baae0aeeb
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:12:41 -0500

    add content to #change that will create a usrs table with columns for the user's username, email, pasword digest, and timestamps

--- db/migrate/20180801210918_create_users.rb
+++ db/migrate/20180801210918_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username
+      t.text :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
   end
 end
