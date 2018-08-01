8f264eb83ef39e799e7048258effb1b93dd38a04
sgibson47 <sgibson47@gmail.com>
2018-08-01 16:25:19 -0500

    add content to #change that will create an interactions table with columns for the interaction's date, user_id, and timestamps

--- db/migrate/20180801212211_create_interactions.rb
+++ db/migrate/20180801212211_create_interactions.rb
@@ -1,4 +1,10 @@
 class CreateInteractions < ActiveRecord::Migration
   def change
+    create_table :interactions do |t|
+      t.date :date
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
   end
 end
