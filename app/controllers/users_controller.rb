aef30907b52f4e6b786712de3ecf597e3660f38b
sgibson47 <sgibson47@gmail.com>
2018-08-01 17:02:44 -0500

    set @user instance variable equal to the current user in order to pass the current user to the users/show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
+      @user = current_user
       erb :'users/show', , locals: {message: "Please Log Out to create a new user account."}
     else
       erb :'/users/signup'
