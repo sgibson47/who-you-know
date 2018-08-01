ad915b23743c933d56a7090839aadebc43370a2c
sgibson47 <sgibson47@gmail.com>
2018-08-01 17:11:56 -0500

    make minor corrections to get '/signup'route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
   get '/signup' do
     if logged_in?
       @user = current_user
-      erb :'users/show', , locals: {message: "Please Log Out to create a new user account."}
+      erb :'users/show', locals: {message: "Please Log Out to create a new user account."}
     else
-      erb :'/users/signup'
+      erb :'users/signup'
     end
   end
+
+
   
 end
