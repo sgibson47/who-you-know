7fa79ab5cc4f3986f252e75b184bc9a90ea13c31
sgibson47 <sgibson47@gmail.com>
2018-08-02 09:51:51 -0500

    add get '/contacts' route to ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,10 @@
 class ContactsController < ApplicationController
+  get '/contacts' do
+    if logged_in?
+      @user = current_user
+      erb :'contacts/show'
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
 end
