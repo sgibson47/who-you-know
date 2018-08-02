5e4ee951d29f69333548510b9b1280e58cad9965
sgibson47 <sgibson47@gmail.com>
2018-08-02 17:04:23 -0500

    add patch '/contacts/:id' route to update a contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -57,8 +57,37 @@ class ContactsController < ApplicationController
         @user = current_user
         erb :'contacts/index', locals: {message: "You didn't make that contact. You can't edit other people's contacts."} 
       else
+        @user = current_user
         erb :'contacts/index', locals: {message: "No such contact."}
       end
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
+
+  patch '/contacts/:id' do
+    if logged_in?
+      @contact = Contact.find(params[:id])
+      @note = Note.new(params[:note]) if !params["note"]["content"].empty?
+      @interaction = Interaction.new(params[:interaction]) if !params["interaction"]["date"].empty?
+      if @contact && @contact.user != current_user
+        @user = current_user
+        erb :'contacts/index', locals: {message: "You didn't make that contact. You can't edit other people's contacts."} 
+      elsif @contact && @contact.user == current_user
+        @contact.update(params[:contact])
+        @note.save if @note
+        @interaction.save if @interaction
+        if @contact.invalid? || @note.invalid? || @interaction.invalid?
+          erb :"/books/update"
+        else
+          @contact.notes << @note if @note.valid?
+          @contact.interactions << @interaction if @interaction.valid?
+          @contact.save
+          redirect to "/contacts/#{@contact.id}"
+        end
+      else
+        @user = current_user
+        erb :'contacts/index', locals: {message: "No such contact."}
     else
       erb :'users/login', locals: {message: "Please sign in to view content."}
     end
