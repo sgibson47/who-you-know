f703142e56b4e4711d326dc77fe9a321bdeb693f
sgibson47 <sgibson47@gmail.com>
2018-08-03 18:02:40 -0500

    break for the day

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -16,4 +16,72 @@ class NotesController < ApplicationController
       erb :'users/login', locals: {message: "Please sign in to view content."}
     end
   end
+
+  post '/notes' do
+    binding.pry
+    if logged_in?
+      @note= Note.new(params[:note])
+      @contact = Contact.new(params[:contact]) if !params["contact"]["name"].empty?
+      if @note.invalid?
+        @note.save
+        erb :"/notes/new"
+      elsif @contact && @contact.invalid?
+        @contact.save if @contact
+        erb :'/notes/new'
+      elsif @interaction && @interaction.invalid?
+        @interaction.save if @interaction
+        erb :'/notes/new'
+      else
+        @note.user = current_user
+        @note.save
+        redirect to "/notes/#{@note.id}"
+      end
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
+
+  post '/notes' do
+    if logged_in?
+      @note= Note.new(params[:note])
+      @contact = Contact.new(params[:contact]) if !params["contact"]["name"].empty?
+      @interaction = Interaction.new(params[:inteaction]) if !params["interaction"]["date"].empty?
+      if @note.invalid?
+        @note.save
+        erb :"/notes/new"
+      elsif @interaction
+        if @interaction.invalid?
+          @interaction.save
+          erb :'/notes/new'
+        elsif @interaction.valid? && params["interaction"]["id"]
+          erb :"/notes/new", locals: {message: "A note can only be associated with one interaction."}
+        elsif @interaction.valid? && !params["interaction"]["id"]
+          @note.interaction = @interaction
+
+      elsif @contact
+        @contact && params["contact"]["id"]
+        erb :"/notes/new", locals: {message: "A note can only be associated with one interaction."}
+
+
+
+
+      
+      end
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
+end
+
+      elsif @contact && @contact.invalid?
+        @contact.save if @contact
+        erb :'/notes/new'
+      elsif @interaction && @interaction.invalid?
+        @interaction.save if @interaction
+        erb :'/notes/new'
+      else
+        @note.user = current_user
+
+        @note.save
+        redirect to "/notes/#{@note.id}"
 end
