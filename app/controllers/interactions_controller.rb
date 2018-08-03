c4c783cb58fdb38a6f524242f29de9b6cdbc7a79
sgibson47 <sgibson47@gmail.com>
2018-08-03 15:53:46 -0500

    add post '/interactions' route to create a new interaction and redirect to the new interaction's show page if the user is logged in and the info provided by the user will create a valid interaction and note, if a note was made

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -15,4 +15,26 @@ class InteractionsController < ApplicationController
       erb :'users/login', locals: {message: "Please sign in to view content."}
     end
   end
+
+  post '/interactions' do
+    if logged_in?
+      @interaction= Interaction.new(params[:interaction])
+      @note = Note.new(params[:note]) if !params["note"]["content"].empty?
+      if @interaction.invalid?
+        @interaction.save
+        erb :"/interactions/new"
+      elsif @note && @note.invalid?
+        @note.save if !!@note
+        erb :'/interactions/new'
+      else
+        @interaction.user = current_user
+        @note.save if !!@note
+        @interaction.notes << @note if @note
+        @interaction.save
+        redirect to "/interactions/#{@interaction.id}"
+      end
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
 end
