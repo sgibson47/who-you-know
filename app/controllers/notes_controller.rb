a2d01b2574fbc6a971426cecea936fe2e168a227
sgibson47 <sgibson47@gmail.com>
2018-08-03 17:03:15 -0500

    add get '/notes' route that allows a logged in user to view an index of their notes or renders the log in page if user is not logged in

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,10 @@
 class NotesController < ApplicationController
+  get '/notes' do
+    if logged_in?
+      @user = current_user
+      erb :'notes/index'
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
 end
