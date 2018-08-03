3fc57969e70bdc6bcf10816763c3e36a1db471ea
sgibson47 <sgibson47@gmail.com>
2018-08-03 16:23:07 -0500

    add add presence validation for date

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -3,4 +3,6 @@ class Interaction < ActiveRecord::Base
   has_many :contact_interactions, class_name: 'ContactInteractions'
   has_many :contacts, through: :contact_interactions
   has_many :notes
+
+  validates :date, presence: {message: "An interaction must have a date."}
 end
