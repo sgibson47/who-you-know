260d3f26eaedeb096f45c4b7539648ffc62ea4a2
sgibson47 <sgibson47@gmail.com>
2018-08-03 15:40:11 -0500

    add get '/interactions' route to display interactions index to a logged in user or to render the login page if the user is not logged in

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,2 +1,10 @@
 class InteractionsController < ApplicationController
+  get '/interactions' do
+    if logged_in?
+      @user = current_user
+      erb :'interactions/index'
+    else
+      erb :'users/login', locals: {message: "Please sign in to view content."}
+    end
+  end
 end
