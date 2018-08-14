cf9e69a6c68880ec25a862df6ed57106701a7286
sgibson47 <sgibson47@gmail.com>
2018-08-14 15:00:54 -0500

    add flash message to #redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,8 @@ class ApplicationController < Sinatra::Base
 
     def redirect_if_not_logged_in
       if !logged_in?
-        redirect to '/login'
+        flash[:alert] = "Please sign in to view content."
+        redirect to '/login' 
       end
     end
   end
